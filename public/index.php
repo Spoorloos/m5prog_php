ec5451ad1902ae5cbf9d38eae35e6431b465fcc6
Mick Negenman <36791@ma-web.nl>
2024-09-19 13:53:51 +0200

    initial commit

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <? for ($i = 1; $i <= 10; $i++): ?>
+        <h1>Hello World <?= $i ?></h1>
+    <? endfor; ?>
+</body>
+</html>
